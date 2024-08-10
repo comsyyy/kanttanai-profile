a4d76c9ce1bcbf90abb9303ef75f4c87773e1d01
comdev <kanttanaibuntat@gmail.com>
2024-08-10 22:13:05 +0700

    profile

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   }
   return (
     <div className="App">
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"></link>
       <Navbar/>
       <img src="photo/profile.jpg" alt="Profile" className="profile-image" />
       <div className="typewriter-text">
